78a1d9632eaa7e622514a930aef9751b7f8468b0
Suraj Sedai <surajsedai05@gmail.com>
2025-06-05 15:20:34 -0400

    data_queue setup

---
+++ app/data_queue.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class ArticleQueue:
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self, article):
+        self.queue.append(article)
+    
+    def dequeue(self):
+        return self.queue.popleft() if self.queue else None
+    
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+class SeenURLs:
+    def __init__(self):
+        self.seen = set()
+    
+    def has_seen(self, url):
+        return url in self.seen
+
+    def add(self,url):
+        self.seen.add(url)
